2acd1f69874cae5d0650e1aa336eda963f0cb672
Morxidia <morxidia@gmail.com>
2026-05-19 20:16:58 +0700

    init

---
+++ src/main/java/com/mycompany/management_alat_camping/Management_alat_camping.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.management_alat_camping;
+
+/**
+ *
+ * @author morxidia
+ */
+public class Management_alat_camping {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
